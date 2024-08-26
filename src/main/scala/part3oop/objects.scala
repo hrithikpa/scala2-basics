405f0c62a63e6785863bffd96d3aa92c261b1122
hrithikpa <hrithikpandey09@gmail.com>
2024-08-26 19:16:09 +0530

    updated objects

--- src/main/scala/part3oop/objects.scala
+++ src/main/scala/part3oop/objects.scala
@@ -3,17 +3,25 @@ package part3oop
 object objects extends App {
   // An object is a class that has exactly one instance.
   // Objects do not receive parameters
-  object Logger {
+  object Person{ // type + it's only instance
     // "static"/"class" - level functionality
-    def info(message: String): Unit = println(s"INFO: $message")
+    val N_EYES = 2
+    def canFly: Boolean = false
     // factory methods
-    def apply(message: String): Logger = new Logger(message)
+    def apply(mother: Person, father: Person): Person = new Person("Bob")
   }
-  class Logger(val message: String) {
+  class Person(val name: String) {
     // instance-level functionality
   }
   // An object with the same name as a class is called a companion object.
-  Logger.info("Successful")
-  val log = Logger.apply("Failed")
-  println(log.message)
+
+  // Scala object = Singleton instance
+  val person1 = Person
+  val person2 = Person
+  println(person1 == person2)
+
+  val marry = new Person("Marry") // this is calling class
+  val john = new Person("John")
+
+  val bob = Person(marry, john) // Similar to Person.apply(marry, john)
 }
